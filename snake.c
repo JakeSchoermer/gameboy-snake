dec636f78ffda7fed9a778ea477335ea793f5541
Jake Schoermer <jake.schoermer@gmail.com>
2021-12-30 23:00:09 +1000

    add cherry sprite

--- snake.c
+++ snake.c
@@ -1,19 +1,19 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "snake.h"
 #include "brick.c"
 #include "border_map.c"
 #include "blankscreen.c"
 #include "character_sprite.c"
+#include "cherry_sprite.c"
 
-UINT8 CHARACTER_SPRITE_ADDR = 0;
+UINT8 CHARACTER_SPRITE_X_COORD = X_OFFSET + 8 * 8;
+UINT8 CHARACTER_SPRITE_Y_COORD = Y_OFFSET + 8 * 7;
 
-UINT8 CHARACTER_SPRITE_X_COORD = 16;
-UINT8 CHARACTER_SPRITE_Y_COORD = 24;
-
-UINT8 CHARACTER_SPRITE_MIN_X = 16;
-UINT8 CHARACTER_SPRITE_MAX_X = 16 + 8 * 17;
-UINT8 CHARACTER_SPRITE_MIN_Y = 24;
-UINT8 CHARACTER_SPRITE_MAX_Y = 24 + 8 * 15;
+UINT8 CHARACTER_SPRITE_MIN_X = X_OFFSET;
+UINT8 CHARACTER_SPRITE_MAX_X = X_OFFSET + 8 * 17;
+UINT8 CHARACTER_SPRITE_MIN_Y = Y_OFFSET;
+UINT8 CHARACTER_SPRITE_MAX_Y = Y_OFFSET + 8 * 15;
 
 void init() {
 
@@ -36,6 +36,11 @@ void init() {
     set_sprite_tile(0, 0);
 
     move_sprite(0, CHARACTER_SPRITE_X_COORD, CHARACTER_SPRITE_Y_COORD);
+    
+    //Spawn Cherry Sprites
+    set_sprite_data(1, 1, cherry_sprite);
+    set_sprite_tile(1, 1); 
+    move_sprite(1, X_OFFSET + 2 * 8, Y_OFFSET + 2 * 8);
 
     return;
 }
---
+++ snake.h
@@ -0,0 +1,2 @@
+#define X_OFFSET 16
+#define Y_OFFSET 24
